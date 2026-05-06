e138f72995c584c60ab4003b9c88f0c73480d9b7
Pranjal-747 <spranjal0515@gamil.com>
2026-05-06 07:36:26 +0530

    added CRUD operation involving MOngodb

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
 const app = express();
+
+connectDb();
+
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 app.use("/api/contacts",require("./routes/contactRoutes"));
